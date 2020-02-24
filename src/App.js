3b47e72850a0a0558f0f224089509622337a5327
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-24 21:14:32 +0100

    Add counter example - lifted state

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 
 class App extends React.Component {
   state = {
+    counter: 0,
     showCounter: true,
   };
 
   render() {
     return (
       <div>
-        {this.state.showCounter && <Counter/>}
+        {this.state.showCounter && <Counter counter={this.state.counter} onClick={this.handleClick}/>}
         {/*{this.showCounter ? <Counter/> : undefined}*/}
         <button onClick={this.handleToggle}>Toggle</button>
       </div>
     )
   }
 
-  handleToggle = () => this.setState(state => ({showCounter: !state.showCounter}))
+  handleToggle = () => this.setState(state => ({showCounter: !state.showCounter}));
+  handleClick = () => this.setState(state => ({counter: state.counter + 1}));
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-class Counter extends React.Component {
-  state = {
-    counter: 0,
-  };
-
-  render() {
-    return (
-      <div>
-        Counter: {this.state.counter}<br/>
-        <button onClick={this.handleClick}>Add</button>
-      </div>
-    )
-  }
-
-  handleClick = () => this.setState(state => ({counter: state.counter + 1}))
+function Counter(props) {
+  return (
+    <div>
+      Counter: {props.counter}<br/>
+      <button onClick={props.onClick}>Add</button>
+    </div>
+  )
 }
 
 export {
