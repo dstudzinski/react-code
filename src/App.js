37f8602f8c168c332019a84b2e5cc0896306d72f
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-24 20:43:39 +0100

    Add class component - wrong handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  // state = {
+  //   counter: 0,
+  // };
+
   render() {
     return (
       <div>
-        Hej
+        Hej {this.state.counter}<br/>
+        <button onClick={this.handleClick}>Add</button>
       </div>
     )
+  }
+
+  handleClick() {
+    // problems - missing bind, this.state used to read current state
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   }
 }
 
