71aaa75c23e0afe8b79817bb0508efb210312070
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-25 00:04:58 +0100

    Add SearchView - fetch data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 class App extends React.Component {
   state = {
     searchVal: '',
+    loading: true,
+    posts: undefined,
   };
 
   render() {
@@ -15,8 +17,15 @@ class App extends React.Component {
           <button type="button" onClick={this.handleClear}>Clear</button>
           <button type="submit">Filter</button>
         </form>
+        <div>{this.state.loading && 'Loading...'}</div>
+        <div>{!this.state.loading && JSON.stringify(this.state.posts)}</div>
+        {/* TODO - render list here with PostItem component and key used properly :) */}
       </div>
     )
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
   }
 
   handleClear = () => {
@@ -31,6 +40,21 @@ class App extends React.Component {
   handleInputChange = (event) => {
     this.setState({searchVal: event.target.value})
   };
+
+  fetchPosts = () => {
+    this.setState({loading: true});
+    // code to make actual fetch should be separated from code which sets state (and component)
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(sleep(2000))
+      .then(posts => {
+        this.setState({loading: false, posts,})
+      });
+  }
+}
+
+function sleep(delay) {
+  return data => new Promise((res, rej) => setTimeout(() => res(data), delay))
 }
 
 export default App;
