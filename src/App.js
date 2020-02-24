1af09286bbbfe19475e4b4f9c6e1ea380eabf9fe
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-25 00:44:18 +0100

    Add Portal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import {ComponentWithModal} from './ComponentWithModal'
 
 function App() {
   return (
     <div className="App">
+      {/* modals mox can't be here because you can't render and get element with getElementById in the same render function
+          you have to move getElementById to some child if you want to have modals box part of React app
+      */}
       <div className="App__content">
-        <ComponentWithModal/>
+        <ComponentWithModal renderInside={document.getElementById('modals')}/>
       </div>
-      <ComponentWithModal/>
+      <ComponentWithModal renderInside={document.getElementById('modals')}/>
     </div>
   );
 }
--- src/ComponentWithModal.js
+++ src/ComponentWithModal.js
@@ -1,8 +1,11 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 import './ComponentWithModal.css';
 
 class ComponentWithModal extends React.Component {
+  domNode = document.createElement('div');
+
   state = {
     showModal: false,
   }
@@ -11,18 +14,34 @@ class ComponentWithModal extends React.Component {
     return (
       <div>
         Some component with modal <button onClick={this.handleOpen}>Open</button>
-        {this.state.showModal && <div className="ComponentWithModal__modal">Modal content <button onClick={this.handleClose}>Close</button></div>}
+        {ReactDOM.createPortal(this.getModal(), this.domNode)}
       </div>
     )
   }
 
+  componentDidMount() {
+    this.props.renderInside.appendChild(this.domNode);
+  }
+
+  componentWillUnmount() {
+    this.props.renderInside.removeChild(this.domNode);
+  }
+
+  getModal = () => {
+    return (
+      <div>
+        {this.state.showModal && <div className="ComponentWithModal__modal">Modal content <button onClick={this.handleClose}>Close</button></div>}
+      </div>
+    )
+  };
+
   handleOpen = () => {
     this.setState({showModal: true})
-  }
+  };
 
   handleClose = () => {
     this.setState({showModal: false})
-  }
+  };
 }
 
 export {
