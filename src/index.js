c230b786b22f2e01a84aa9cdfaad30a9d00460bd
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-24 20:29:29 +0100

    Add Button component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Button} from "./Button";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Button onClick={() => alert('Hej')}>Kliknij</Button>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
