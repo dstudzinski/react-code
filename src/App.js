0b464d83ddb381c9c4189e2efb101147b4ce7cd8
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-24 23:52:39 +0100

    Add SearchView

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    searchVal: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFilter}>
+          <input value={this.state.searchVal} onChange={this.handleInputChange}/>
+          <button type="button" onClick={this.handleClear}>Clear</button>
+          <button type="submit">Filter</button>
+        </form>
+      </div>
+    )
+  }
+
+  handleClear = () => {
+    this.setState({searchVal: ''})
+  };
+
+  handleFilter = (event) => {
+    event.preventDefault();
+    alert(this.state.searchVal);
+  };
+
+  handleInputChange = (event) => {
+    this.setState({searchVal: event.target.value})
+  };
 }
 
 export default App;
