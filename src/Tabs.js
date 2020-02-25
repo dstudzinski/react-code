c6154ea7c7fd5bd2f42cba16c0cb395071c887f5
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-25 21:34:44 +0100

    Add Tabs - cloneElement

--- src/Tabs.js
+++ src/Tabs.js
@@ -3,12 +3,15 @@ import React from "react";
 class Tabs extends React.Component {
   render() {
     const filteredTabs = React.Children.toArray(this.props.children).filter(child => child.type.displayName === 'Tab');
+    const selectedTabCmp = filteredTabs.filter(tab => tab.props.id === this.props.tabSelected)[0];
 
     return (
       <div>
         <div>Select tab: {this.getTabsButtons(filteredTabs)}</div>
         <div>
-          {filteredTabs.filter(tab => tab.props.id === this.props.tabSelected)}
+          {
+            React.cloneElement(selectedTabCmp, {}, selectedTabCmp.props.children + ' injected')
+          }
         </div>
       </div>
     )
