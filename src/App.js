7dd388e15b2a919e626cad828afddd711a51ee51
Dawid Studziński <dmstudzinski@gmail.com>
2020-02-27 23:07:09 +0100

    Add React.lazy and React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const First = React.lazy(() => import("./First"));
+const Second = React.lazy(() => import("./SecondReexport"));
+
+class App extends React.Component {
+  state = {
+    show: false,
+  };
+
+  render() {
+    return (
+      <div>
+        Open network tab and enable throttling before button click and check what will be downloaded after click<br/>
+        <button onClick={this.handleShow}>Show components</button><br/>
+        {this.state.show && (
+          <div className="App">
+            <Suspense fallback={'Loading...'}>
+              <First/>
+            </Suspense>
+            <Suspense fallback={<First/>}>
+              <Second/>
+            </Suspense>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  handleShow = () => this.setState({show: true});
 }
 
 export default App;
---
+++ src/First.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function First() {
+  return (
+    <div>First</div>
+  )
+}
+
+export default First;
